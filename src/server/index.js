bf8ef24270ce724cdbc6ea2110bda6711f27c1aa
whitehatjoker <rkhudoyberdiev@gmail.com>
2020-07-21 03:22:52 +0500

    Add aylien

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,7 @@
 var path = require('path')
 const express = require('express')
+
+
 const mockAPIResponse = require('./mockAPI.js')
 
 const app = express()
@@ -7,6 +9,13 @@ const app = express()
 app.use(express.static('dist'))
 
 console.log(__dirname)
+// Require the Aylien npm package
+var aylien = require("aylien_textapi");
+// You could call it aylienapi, or anything else
+var textapi = new aylien({
+    application_id: "your-api-id",
+    application_key: "your-key"
+  });
 
 app.get('/', function (req, res) {
     // res.sendFile('dist/index.html')
@@ -14,8 +23,8 @@ app.get('/', function (req, res) {
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(8081, function () {
+    console.log('Example app listening on port 8081!')
 })
 
 app.get('/test', function (req, res) {
