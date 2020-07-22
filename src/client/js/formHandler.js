6c16b210f26ad70a9f372658a1dc058806a4c825
whitehatjoker <rkhudoyberdiev@gmail.com>
2020-07-23 03:59:11 +0500

    fix async function

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,21 +1,18 @@
 function handleSubmit(event) {
     event.preventDefault()
-
+    
     // check what text was put into the form field
-    let formText = document.getElementById('name').value
+    let formText = document.getElementById('name').value;
 
     const baseURL = 'https://api.meaningcloud.com/sentiment-2.1?key=';
     const params = '&lang=en&of=json&txt=';
     
-    Client.getAppKey()
-    .then(function(data) {
-        Client.postMeaningCloudApi(baseURL, data.applicationKey, params, formText)
+    Client.getAppKey().then(function(data) {
+        Client.postMeaningCloudApi(baseURL, data.applicationKey, params, formText).then(function(data) {
+            console.log(data);
+        })
     })
-    .then(function(data) {
-        console.log(data)
-        document.getElementById('results').innerHTML = data
-    })
-    
+
 }
 
 export { handleSubmit }
