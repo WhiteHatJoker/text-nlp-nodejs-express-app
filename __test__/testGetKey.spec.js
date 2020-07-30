0879ed94bf0a1ea4e6386dae39fa607cd0d0d68d
Ravshan Khudoyberdiev <rkhudoyberdiev@gmail.com>
2020-07-30 21:08:44 +0500

    Remove live app keys

--- __test__/testGetKey.spec.js
+++ __test__/testGetKey.spec.js
@@ -2,8 +2,8 @@
 describe("Testing getting API keys from Express functionality", () => {
     test("Testing the getAppKey() function", async () => {
         const mockUrl = '/appkey';
-        const mockResponse = {"applicationKey": "eb1bd3ab2713b429c1577fcab18b56c6"};
+        const mockResponse = {"applicationKey": "fake app key"};
         const getAppKey = jest.fn(url => mockResponse);
         const response = await getAppKey(mockUrl);
-        expect(response).toEqual({"applicationKey": "eb1bd3ab2713b429c1577fcab18b56c6"});
+        expect(response).toEqual({"applicationKey": "fake app key"});
 })});
